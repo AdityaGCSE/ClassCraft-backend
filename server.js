70791243c8fca9233369712d4bc17636487a4d32
Aditya Ghosh <104888138+AdityaGCSE@users.noreply.github.com>
2023-09-16 14:55:48 +0530

    test

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ app.use(
 	cookieSession({
 		name: "session",
 		keys: ["cyberwolve"],
+		sameSite: "none",
+		domain: ".onrender.com",
+		path: "/",
 		secure: true,
 		maxAge: 24 * 60 * 60 * 100,
 	})
